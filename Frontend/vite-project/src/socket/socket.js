475ccaf1038a97241cd2cdda9a4bfa18d6876419
Shankar lal <cleet1304@gmail.com>
2026-08-16 17:36:53 +0530

    Your commit message here

--- Frontend/vite-project/src/socket/socket.js
+++ Frontend/vite-project/src/socket/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://full-stack-event-wus1.onrender.com";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://code-together-1g0z.onrender.com";
 
 export const socket = io(BACKEND_URL  , {
   autoConnect: true,
