6f1c96f77fad84a6009e3ddedc2141c3d47908d8
Shankar lal <cleet1304@gmail.com>
2026-08-16 17:05:44 +0530

    final commit

--- Backend/src/socket/index.js
+++ Backend/src/socket/index.js
@@ -10,7 +10,7 @@ const registerVoiceHandlers = require("./handlers/voiceHandler");
 function initializeSocket(server){
     const io = new Server(server,{
         cors:{
-            origin:"http://localhost:5173",
+            origin:"https://codetogether-mu.vercel.app",
             credentials:true
         }
     });
--- Frontend/vite-project/src/socket/socket.js
+++ Frontend/vite-project/src/socket/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://full-stack-event-wus1.onrender.com";
 
-export const socket = io(BACKEND_URL , {
+export const socket = io(BACKEND_URL  , {
   autoConnect: true,
   transports: ["websocket", "polling"]
 });
